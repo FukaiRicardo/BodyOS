f3c3d7401e917fe40a71270c0a4b58664dbb67c9
Ricardo Fukai <fukairicardo@gmail.com>
2026-05-24 01:04:58 +0900

    fix: replace pino with simple logger in gateway

--- services/gateway/src/logger.ts
+++ services/gateway/src/logger.ts
@@ -1,35 +1,9 @@
-import pino from 'pino'
+export const logger = {
+  info: (...args: any[]) => console.log('[INFO]', ...args),
+  warn: (...args: any[]) => console.warn('[WARN]', ...args),
+  error: (...args: any[]) => console.error('[ERROR]', ...args),
+  debug: (...args: any[]) => console.debug('[DEBUG]', ...args),
+}
 
-const logLevel = process.env.LOG_LEVEL || 'info'
-
-export const logger = pino({
-  level: logLevel,
-  transport: process.env.NODE_ENV === 'production'
-    ? undefined
-    : {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          singleLine: false,
-          translateTime: 'SYS:standard',
-          ignore: 'pid,hostname'
-        }
-      }
-})
-
-export const pinoHttpLogger = require('pino-http')({
-  logger: logger,
-  customLogLevel: (req: any, res: any, err: any) => {
-    if (res.statusCode >= 500) return 'error'
-    if (res.statusCode >= 400) return 'warn'
-    return 'info'
-  },
-  customSuccessMessage: (req: any, res: any) => {
-    return `${req.method} ${req.url} - ${res.statusCode}`
-  },
-  customErrorMessage: (req: any, res: any, err: any) => {
-    return `${req.method} ${req.url} - ${res.statusCode} - ${err?.message}`
-  }
-})
-
+export const pinoHttpLogger = (_req: any, _res: any, next: any) => next()
 export default logger

462285514c3f57e0754093172cdf2f84e24bd480
Ricardo Fukai <fukairicardo@gmail.com>
2026-06-02 20:48:55 +0900

    fix: ignore opentelemetry dynamic import incompatible with hermes

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,4 +2,11 @@ const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 const config = getSentryExpoConfig(__dirname);
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.includes('@opentelemetry')) {
+    return { type: 'empty' };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
